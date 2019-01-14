eb7e6087ba914484913a7108f9290c7daa27046a
fuhao <1643091610@qq.com>
2019-01-15 05:24:10 +0800

    配置

---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+import Home from '@/views/Home.vue'
+
+export default [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    path: '/argu/:name',
+    component: () => import('@/views/argu.vue')
+  }
+]
